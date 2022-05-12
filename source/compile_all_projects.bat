bde35a605fdf999333c7ebb54af06482d542fc21
Jaycie Erysdren <jaycie.ewald@outlook.com>
2022-05-12 18:11:50 -0500

    big code reorganization to keep me sane

---
+++ source/compile_all_projects.bat
@@ -0,0 +1,10 @@
+echo #define STANLEY > project.src
+start /W fteqcc64 progs.src
+
+echo #define DUKE > project.src
+start /W fteqcc64 progs.src
+
+echo #define PLATFORM > project.src
+start /W fteqcc64 progs.src
+
+pause
